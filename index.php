8707c2ee8ca1b5a095bf1124b240a531cc905040
Rarex1 <danny.trabajos@gmail.com>
2017-05-22 22:46:41 -0400

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>First Sight</title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="#">Iniciar sesion</a></li>
+			<li><a href="#">Registrarse</a></li>
+			<input type="text" placeholder="Buscar">
+		</ul>
+	</nav>
+	<section class="dashboard">
+		<div class="foto"></div>
+		<div class="foto"></div>
+		<div class="foto"></div>
+		<div class="foto"></div>
+		<div class="foto"></div>
+	</section>
+</body>
+</html>
